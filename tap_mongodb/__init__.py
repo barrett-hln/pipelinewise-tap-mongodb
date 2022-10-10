af2f8c64ac8c96012dab9259e6005c71daeb2f3f
Barrett Snyder <barrett@hln.com>
2022-10-10 19:37:34 +0000

    eval config read booleans as boolean type or lower string

--- tap_mongodb/__init__.py
+++ tap_mongodb/__init__.py
@@ -253,11 +253,11 @@ def get_connection_string(config: Dict):
 
     Returns: A MongoClient connection string
     """
-    srv = config.get('srv') == 'true'
+    srv = config.get('srv') is True or config.get('srv') == 'true'
 
     # Default SSL verify mode to true, give option to disable
-    verify_mode = config.get('verify_mode', 'true') == 'true'
-    use_ssl = config.get('ssl') == 'true'
+    verify_mode = config.get('verify_mode') is True or config.get('verify_mode', 'true') == 'true'
+    use_ssl = config.get('ssl') is True or config.get('ssl') == 'true'
 
     connection_query = {
         'readPreference': 'secondaryPreferred',
@@ -273,7 +273,7 @@ def get_connection_string(config: Dict):
     # NB: "sslAllowInvalidCertificates" must ONLY be supplied if `SSL` is true.
     if not verify_mode and use_ssl:
         connection_query['tlsAllowInvalidCertificates'] = 'true'
-        
+
     connection_query['authMechanism'] = config.get('auth_mechanism', 'SCRAM-SHA-256')
 
     query_string = parse.urlencode(connection_query)
@@ -284,6 +284,8 @@ def get_connection_string(config: Dict):
                         f'{config["password"]}@{config["host"]}' \
                         f'{port}/{config["database"]}?{query_string}'
 
+    LOGGER.debug('connecting with: %s', connection_string)
+
     return connection_string
 
 
@@ -293,7 +295,7 @@ def main_impl():
     """
     args = utils.parse_args(REQUIRED_CONFIG_KEYS)
     config = args.config
-    srv = config.get('srv') == 'true'
+    srv = config.get('srv') is True or config.get('srv') == 'true'
 
     if not srv:
         args = utils.parse_args(REQUIRED_CONFIG_KEYS_NON_SRV)
@@ -307,7 +309,7 @@ def main_impl():
                 client.server_info().get('version', 'unknown'))
 
     common.INCLUDE_SCHEMAS_IN_DESTINATION_STREAM_NAME = \
-        (config.get('include_schemas_in_destination_stream_name') == 'true')
+        (config.get('include_schemas_in_destination_stream_name') is True or config.get('include_schemas_in_destination_stream_name') == 'true')
 
     if args.discover:
         do_discover(client, config)
